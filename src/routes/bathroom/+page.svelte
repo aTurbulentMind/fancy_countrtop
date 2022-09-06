dc2c3c2a7c10cff59057dc1b8914f7abc48c2193
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-09-06 15:29:52 -0700

    Add files via upload

---
+++ src/routes/bathroom/+page.svelte
@@ -0,0 +1,61 @@
+<body>
+    
+
+
+<h1 class="section-title">words goin sideways crazy</h1>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, animi itaque. Iste expedita eaque dicta itaque sapiente enim excepturi veniam voluptate nemo, consequatur commodi ullam perspiciatis maiores totam ratione, quas labore debitis deserunt beatae ex. Labore illum provident quos totam dolor iste repudiandae placeat explicabo. Laboriosam reiciendis voluptatibus nam vero!</p>
+
+<h2>This project</h2>
+<div class="media-scroller snaps-inline">
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+</div>
+
+<h2>that project</h2>
+
+<div class="media-scroller snaps-inline">
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+</div>
+
+<h2>this other project</h2>
+
+<div class="media-scroller snaps-inline">
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    <div class="media-element"><img src="/img/tealish.jpg" alt=""></div>
+    
+</div>
+</body>
+
+<style lang="scss">
+    @import '../src/lib/basecamp';
+    @import '../src/lib/shows';
+</style>
